9decb2b020f548c6114aa2510c942fbcbb462e87
balcse <cserpesbalazs@gmail.com>
2024-12-12 15:16:31 +0100

    fix: update convert_bim: - created a function for transforming geojsons

--- infrared_wrapper_api/api/convert_bim.py
+++ infrared_wrapper_api/api/convert_bim.py
@@ -4,77 +4,81 @@ import shapely
 from dotbimpy import *
 import uuid
 
-filename = 'Updated_Zhytomyr-buildings_no_type_change_Dayanna_30.10'
+filename = 'geoJson-Leipzig-01102024'
 path_buildings = f'infrared_wrapper_api/models/jsons/{filename}.json'
-
 gdf = gpd.read_file(path_buildings)
 
-# Rename B_height in case of Chernivtsi file
-gdf = gdf.rename(columns={'B_height':'building_height'})
-gdf = gdf.loc[~gdf['building_height'].isna()]
-gdf = gdf.explode()
-gdf = gdf.to_crs('EPSG:25832')
-centroid = gdf['geometry'].union_all().centroid
-centroid = shapely.get_coordinates(centroid).tolist()[0]
-c_x = centroid[0]
-c_y = centroid[1]
-meshes = []
-elements = []
 
-counter = 0
-for _, row in gdf.iterrows():
-    geometry = row['geometry']
-    height = row['building_height']
+def convert_geojsons(gdf: gpd.GeoDataFrame, height_col: str):
+    gdf = gdf.rename(columns={height_col:'building_height'})
+    gdf = gdf.loc[~gdf['building_height'].isna()]
+    gdf = gdf.explode()
+    gdf = gdf.to_crs('EPSG:25832')
+    centroid = gdf['geometry'].union_all().centroid
+    centroid = shapely.get_coordinates(centroid).tolist()[0]
+    c_x = centroid[0]
+    c_y = centroid[1]
+    meshes = []
+    elements = []
 
-    # Skip invalid or empty geometries
-    if geometry.is_empty or not geometry.is_valid:
-        print("Skipping invalid or empty geometry")
-        continue
+    counter = 0
+    for _, row in gdf.iterrows():
+        geometry = row['geometry']
+        height = row['building_height']
 
-    # Fix precision issues and check area
-    geometry = geometry.buffer(0)
-    if geometry.area == 0 or len(geometry.exterior.coords) < 3:
-        print("Skipping degenerate or zero-area geometry")
-        continue
+        # Skip invalid or empty geometries
+        if geometry.is_empty or not geometry.is_valid:
+            print("Skipping invalid or empty geometry")
+            continue
 
-    # Remove duplicate closing point
-    exterior = [(x-c_x, y-c_y, 0) for x, y in geometry.exterior.coords]
-    if exterior[0] == exterior[-1]:
-        exterior = exterior[:-1]
+        # Fix precision issues and check area
+        geometry = geometry.buffer(0)
+        if geometry.area == 0 or len(geometry.exterior.coords) < 3:
+            print("Skipping degenerate or zero-area geometry")
+            continue
 
-    # Check bounds to skip degenerate polygons
-    x_coords, y_coords = zip(*[(x, y) for x, y, _ in exterior])
-    if max(x_coords) - min(x_coords) == 0 or max(y_coords) - min(y_coords) == 0:
-        print("Degenerate geometry: all points lie on a line or single point")
-        continue    
+        # Remove duplicate closing point
+        exterior = [(x-c_x, y-c_y, 0) for x, y in geometry.exterior.coords]
+        if exterior[0] == exterior[-1]:
+            exterior = exterior[:-1]
 
-    # Create and triangulate PolyData
-    base_polygon = pv.PolyData(exterior).delaunay_2d()
-    
-    extrusion = base_polygon.extrude([0, 0, height], capping=True)
-    extrusion = extrusion.triangulate()
-    
+        # Check bounds to skip degenerate polygons
+        x_coords, y_coords = zip(*[(x, y) for x, y, _ in exterior])
+        if max(x_coords) - min(x_coords) == 0 or max(y_coords) - min(y_coords) == 0:
+            print("Degenerate geometry: all points lie on a line or single point")
+            continue    
 
-    extrusion = extrusion.extract_surface()
-    coordinates = extrusion.points.flatten().tolist()
-    indices = extrusion.regular_faces.flatten().tolist()
+        # Create and triangulate PolyData
+        base_polygon = pv.PolyData(exterior).delaunay_2d()
+        
+        extrusion = base_polygon.extrude([0, 0, height], capping=True)
+        extrusion = extrusion.triangulate()
+        
 
-    vector = Vector(x=0,y=0,z=0)
-    rotation = Rotation(qx=0, qy=0, qz=0, qw=1.0)
-    geomtype = 'Block'
-    color = Color(r=120, g=166, b=171, a=180)
-    info = {'Name':f'building-{counter}'}
-    guid = str(uuid.uuid4())
+        extrusion = extrusion.extract_surface()
+        coordinates = extrusion.points.flatten().tolist()
+        indices = extrusion.regular_faces.flatten().tolist()
 
-    mesh = Mesh(mesh_id=counter,coordinates=coordinates,indices=indices)
-    element = Element(mesh_id=counter,vector=vector,rotation=rotation,guid=guid,type=geomtype,color=color,info=info)
-    meshes.append(mesh)
-    elements.append(element)
-    counter=counter+1
+        vector = Vector(x=0,y=0,z=0)
+        rotation = Rotation(qx=0, qy=0, qz=0, qw=1.0)
+        geomtype = 'Block'
+        color = Color(r=120, g=166, b=171, a=180)
+        info = {'Name':f'building-{counter}'}
+        guid = str(uuid.uuid4())
 
-file_info = {
-    "Author":"DCS"
-}
+        mesh = Mesh(mesh_id=counter,coordinates=coordinates,indices=indices)
+        element = Element(mesh_id=counter,vector=vector,rotation=rotation,guid=guid,type=geomtype,color=color,info=info)
+        meshes.append(mesh)
+        elements.append(element)
+        counter=counter+1
 
-file = File("1.0.0",meshes=meshes,elements=elements,info=file_info)
-file.save(f"{filename}.bim")
+    file_info = {
+        "Author":"DCS"
+    }
+
+    file = File("1.0.0",meshes=meshes,elements=elements,info=file_info)
+    file.save(f"{filename}.bim")
+
+
+
+convert_geojsons(gdf=gdf,height_col='building_height')
